ed3f5d4e6a3f270c99895248af9927af2ae92b5f
KevinJeuter <64601202+KevinJeuter@users.noreply.github.com>
2020-07-17 02:52:33 +0200

    Add files via upload

--- project/src/ast/Node.java
+++ project/src/ast/Node.java
@@ -6,11 +6,6 @@ import visitors.Visitor;
 public abstract class Node implements Cloneable{
 	
 	public abstract Node accept(Visitor v); //node
-	
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
 
-	    return super.clone();
-	}
 	
 }
